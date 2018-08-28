e1f03a6ba3d9ec6a003dc0495ec57e86c80a65be
Jonatan Selsing <jselsing@dark-cosmology.dk>
2018-08-28 11:23:20 +0200

    Arxiv submission

--- py/database/timediff.py
+++ py/database/timediff.py
@@ -12,11 +12,11 @@ def timediff(t1, t2):
     time2 = datetime.datetime(int(t2[0:4]), int(t2[5:7]), int(t2[8:10]), hour=int(round(float(t2[11:13]))), minute=int(round(float(t2[14:16]))), second=int(round(float(t2[17:19]))))
 
     dt = time2 - time1
-    return dt.total_seconds()/(3600)
+    return dt.total_seconds()/(60)
 
 if __name__ == '__main__':
 
-    t_trigger = "2016:12:19T18:48:39"
-    t_obs = "2016-12-21T06:33:30.354"
+    t_trigger = "2018:04:04T00:45:35"
+    t_obs = "2018:04:04T01:01:33"
 
     print(timediff(t_trigger, t_obs))
